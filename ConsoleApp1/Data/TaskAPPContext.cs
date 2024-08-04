cefe108d0c9f79d301e7754a093c1540b3c4945b
sohaibaltrefe <sohaibshaltafaltrefe1@gmail.com>
2024-08-04 13:32:33 +0300

    Add project files.

---
+++ ConsoleApp1/Data/TaskAPPContext.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp1.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Data
+{
+    internal class TaskAPPContext  :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=task2ef;Trusted_Connection=True;TrustServerCertificate=true");
+        }
+        DbSet<Product>products { set; get; }
+    }
+}
